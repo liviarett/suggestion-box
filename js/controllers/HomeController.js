33d7aa98a157d394c80ffa9350f13c786ad5c552
Livia Rett <livia.rett@gmail.com>
2017-10-15 17:49:21 +0100

    submit functionality done

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,3 +1,19 @@
-app.controller('HomeController', ['$scope', function($scope) {
-	$scope.helloWorld = "Hello, AngularJS!";
+app.controller('HomeController', ['$scope', 'suggestions', function($scope, suggestions) {
+	$scope.posts = suggestions.posts;
+	$scope.addSuggestion = function() {
+		//if input empty, don't submit
+		if(!$scope.title || $scope.title === "") {
+			return;
+		} 
+		console.log($scope.title);
+		//push suggestion posts in suggestions.js
+		$scope.posts.push({
+			title: $scope.title,
+			upvotes: 0,
+			comments: []
+		});
+		//after submit, clear input
+		$scope.title = "";
+		console.log($scope.posts);
+	};
 }]);
