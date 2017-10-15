3b1ca675a99a0dd983cf2c0ada62d8e44cccd2d3
Livia Rett <livia.rett@gmail.com>
2017-10-15 23:38:56 +0100

    main project finished

--- js/controllers/PostController.js
+++ js/controllers/PostController.js
@@ -1,9 +1,12 @@
 app.controller('PostController', ['$scope', '$routeParams', 'suggestions', function($scope, $routeParams, suggestions) {
 	$scope.post = suggestions.posts[$routeParams.id];
 	
-	$scope.addComment = function(comment) {
-		post.comments.push({
-			body: comment,
+	$scope.addComment = function() {
+		if(!$scope.body || $scope.body === "") {
+return;
+		}
+		$scope.post.comments.push({
+			body: $scope.body,
 			upvotes: 0
 		})
 	}
--- js/services/suggestions.js
+++ js/services/suggestions.js
@@ -4,24 +4,39 @@ app.service('suggestions', function() {
             title: 'Free pizza at club meetings',
             upvotes: 8,
             comments: [{
-				body:'hello',
-				upvotes: 0
+				body: 'Love the idea!',
+				upvotes: 9
+			}, {
+				body: 'Maybe we should have a healthier option?',
+				upvotes: 1
 			}],
             id: 0
         }, {
             title: 'End all club emails with Laffy Taffy jokes',
             upvotes: 9,
-            comments: [],
+            comments: [{
+				body: 'That would be funny!',
+				upvotes: 5
+			}],
             id: 1
         }, {
             title: 'Retrofit water fountain with Gatorade',
             upvotes: 7,
-            comments: [],
+            comments: [{
+				body: 'That\'s the most ridiculous idea I have ever heard',
+				upvotes: 7
+			}, {
+				body: 'This is meant to be a serious discussion!',
+				upvotes: 2
+			}],
             id: 2
         }, {
             title: 'Sing Bon Jovi\'s "Living on a Prayer" halfway through meetings',
             upvotes: 3,
-            comments: [],
+            comments: [{
+				body: 'haha great',
+				upvotes: 0
+			}],
             id: 3
         }]
     };
